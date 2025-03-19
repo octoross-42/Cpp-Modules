57ac37ed11f57f5638cbfcaac8230dd47c2f5d61
octoross <codingoceane@gmail.com>
2025-03-19 19:23:13 +0100

    CPP04: fixed every copy of child masking protected value _type

--- CPP Module 04/ex01/Dog.hpp
+++ CPP Module 04/ex01/Dog.hpp
@@ -6,7 +6,7 @@
 /*   By: octoross <octoross@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/18 14:36:14 by octoross          #+#    #+#             */
-/*   Updated: 2025/03/18 17:23:27 by octoross         ###   ########.fr       */
+/*   Updated: 2025/03/19 19:14:15 by octoross         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,6 @@ class	Dog: public Animal
 {
 	private:
 		Brain	*_brain;
-	protected:
-		std::string	_type;
 	public:
         Dog(void);
 		~Dog(void);
