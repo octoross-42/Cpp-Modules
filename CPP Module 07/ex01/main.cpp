d45eb49ca0c58a978a6b8867c97fb2d304d682b9
octoross-42 <codingcoeane@gmail.com>
2025-05-20 21:28:12 +0200

    Rm unecessary

--- CPP Module 07/ex01/main.cpp
+++ CPP Module 07/ex01/main.cpp
@@ -4,7 +4,7 @@ int main(void)
 {
 	int arri[5] = { 1, 2, 3, 4, 5};
 	char s[5] = { 'c', 'h', 'a', 'r', '\0' };
-	::iter(arri, 5, test);
-	::iter(s, 5, test);
-	::iter((char *)NULL, 5, test);
+	iter(arri, 5, test);
+	iter(s, 5, test);
+	iter((char *)NULL, 5, test);
 }
