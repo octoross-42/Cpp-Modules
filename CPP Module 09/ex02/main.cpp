6a8cfb7a5cbc71e4dff9669a3d4e6e346fa25361
octoross-42 <codingcoeane@gmail.com>
2025-06-07 16:21:08 +0200

    Changed error time to throw runtime_exception

--- CPP Module 09/ex02/main.cpp
+++ CPP Module 09/ex02/main.cpp
@@ -5,7 +5,7 @@ double getRealTime(void)
 {
     timespec ts;
     if (clock_gettime(CLOCK_MONOTONIC, &ts) == -1)
-		return (-42);
+		throw std::runtime_error("Couldnt get time");
     return (ts.tv_sec * 1000.0 + ts.tv_nsec / 1000000.0);
 }
 
